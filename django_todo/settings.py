88df2af2401b633b83455c8785de75c45e3057ba
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-04-08 12:19:44 +0000

    Fix allowed hosts in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fkbab++_5p+ms0!whqzc#=l$kbim=r)^&v0w1j*id=4kdef2%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gareth-django-todo-app.herokuapp.com']
 
 
 # Application definition
